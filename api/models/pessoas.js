919d690b9da4e17f1bfb8eedbc78ff778f4bfd80
Antônio Evaldo <antonio.evaldo349@gmail.com>
2022-12-13 09:37:58 -0300

    video-1.3: adicionando paranoid (soft delete)

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,26 +1,30 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class Pessoas extends Model {
     static associate(models) {
       Pessoas.hasMany(models.Turmas, {
-        foreignKey: 'docente_id'
+        foreignKey: "docente_id",
       });
       Pessoas.hasMany(models.Matriculas, {
-        foreignKey: 'estudante_id'
+        foreignKey: "estudante_id",
       });
     }
   }
 
-  Pessoas.init({
-    nome: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Pessoas',
-  });
+  Pessoas.init(
+    {
+      nome: DataTypes.STRING,
+      ativo: DataTypes.BOOLEAN,
+      email: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Pessoas",
+      paranoid: true,
+    }
+  );
   return Pessoas;
 };
