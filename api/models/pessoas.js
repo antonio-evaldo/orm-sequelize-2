c2e4e09e16b68d75d38c586d841a8f4c9fcb20d1
Antônio Evaldo <antonio.evaldo349@gmail.com>
2022-12-13 10:57:06 -0300

    video-2.1: declarando escopo padrão no modelo

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -24,6 +24,9 @@ module.exports = (sequelize, DataTypes) => {
       sequelize,
       modelName: "Pessoas",
       paranoid: true,
+      defaultScope: {
+        where: { ativo: true },
+      },
     }
   );
   return Pessoas;
